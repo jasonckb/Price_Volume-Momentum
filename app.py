b662af509c4fb2e834836f5b767e3abd59114871
jasonckb <122160477+jasonckb@users.noreply.github.com>
2024-03-04 21:40:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ import datetime
 # Dropbox direct download link
 excel_path = 'https://www.dropbox.com/scl/fi/nw5fpges55aff7x5q3gh9/Index-Weight.xlsx?rlkey=rxdopdklplz15jk97zu2sual5&dl=1'
 
+@st.cache(show_spinner=False, allow_output_mutation=True)
 def load_data(excel_path, force_reload=False):
     sheet_names = ['HSI', 'HSTECH', 'HSCEI','SP 500']
     dtype = {'Code': str}
     return {name: pd.read_excel(excel_path, sheet_name=name, dtype=dtype) for name in sheet_names}
 
+@st.cache(show_spinner=False, allow_output_mutation=True)
 def fetch_and_calculate(df, index_name):
     for index, row in df.iterrows():
         # Adjust the stock code format based on the index
@@ -45,9 +47,12 @@ def main():
     st.set_page_config(page_title="Index Constituents Volume & Price Momentum by Jason Chan", layout="wide")
     st.title('Index Components Volume & Price Momentum by Jason Chan')
     # Refresh button in the sidebar
-    if st.sidebar.button('Refresh Data'):        
-        st.experimental_rerun()
+    # Provide a mechanism to force data reload
+    if 'force_reload' not in st.session_state:
+        st.session_state['force_reload'] = False
 
+    if st.sidebar.button('Refresh Data'):
+        st.session_state['force_reload'] = not st.session_state['force_reload']
     # Fetch the raw data only once and deep copy any data frame you retrieve for manipulation
     if 'raw_data' not in st.session_state:
         st.session_state.raw_data = load_data(excel_path)
