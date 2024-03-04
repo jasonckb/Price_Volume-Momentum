ec3c779de40c004b838a25b4ef69db61309083d9
jasonckb <122160477+jasonckb@users.noreply.github.com>
2024-03-04 21:46:03 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -47,12 +47,9 @@ def main():
     st.set_page_config(page_title="Index Constituents Volume & Price Momentum by Jason Chan", layout="wide")
     st.title('Index Components Volume & Price Momentum by Jason Chan')
     # Refresh button in the sidebar
-    # Provide a mechanism to force data reload
-    if 'force_reload' not in st.session_state:
-        st.session_state['force_reload'] = False
+    if st.sidebar.button('Refresh Data'):        
+        st.experimental_rerun()
 
-    if st.sidebar.button('Refresh Data'):
-        st.session_state['force_reload'] = not st.session_state['force_reload']
     # Fetch the raw data only once and deep copy any data frame you retrieve for manipulation
     if 'raw_data' not in st.session_state:
         st.session_state.raw_data = load_data(excel_path)
@@ -64,8 +61,13 @@ def main():
     index_choice = st.sidebar.selectbox('Select Index', list(processed_data.keys()))
     df_display = processed_data[index_choice].copy(deep=True)  # Ensuring another deep copy
 
-    for name, df in processed_data.items():
-        df['Today Pct Change'] = df['Today Pct Change'].apply(format_pct_change)
+     # Convert 'Today Pct Change' to numeric, handling any non-numeric values gracefully
+    df_display['Today Pct Change'] = pd.to_numeric(df_display['Today Pct Change'].str.rstrip('%'), errors='coerce')
+
+    # After conversion, calculate the max percentage change
+    max_pct_change = df_display['Today Pct Change'].max()
+    if max_pct_change is not None:
+        max_pct_change *= 1.1
 
     
     def color_scale(val):
